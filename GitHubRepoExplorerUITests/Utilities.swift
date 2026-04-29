bdc968990c5339df7093cf261c779037f57d3337
abir4u <abirpal90@gmail.com>
2026-04-29 22:51:46 +1200

    Clean up UI tests

---
+++ GitHubRepoExplorerUITests/Utilities.swift
@@ -0,0 +1,13 @@
+//
+//  Utilities.swift
+//  GitHubRepoExplorer
+//
+//  Created by Abir Pal on 29/04/2026.
+//
+
+import XCTest
+
+public func assertElementExists(app: XCUIApplication, identifier: String, errorMessage: String = "", timeout: TimeInterval = 2) {
+    var message = errorMessage == "" ? "Unable to locate element with identifier '\(identifier)'" : errorMessage
+    XCTAssertTrue(app.staticTexts[identifier].waitForExistence(timeout: timeout), message)
+}
